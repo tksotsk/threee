95b9c82cb8604b28a0e8247f9674623744b3aeda
takashi otsuka <shoshi100010000@gmail.com>
2022-10-17 23:14:38 +0900

    fix er chart (conflict)

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2022_10_12_094303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "themes", force: :cascade do |t|
-    t.string "theme"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
