ff477c506fb44496e3e5be907d5ddbfd944f42c0
takashi otsuka <shoshi100010000@gmail.com>
2022-10-18 19:54:07 +0900

    add charts.png

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
   root 'pages#top'
+  devise_for :users
+  resources :projects 
   resources :themes
-  resources :projects
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
