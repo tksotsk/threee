a6e3960b5b214134b6d236c3c51e0c5734e7285b
takashi otsuka <shoshi100010000@gmail.com>
2022-10-20 17:21:53 +0900

    add guest login func

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'pages#top'
-<<<<<<< HEAD
   devise_for :users
-  resources :projects 
-  resources :themes
-  
-=======
+  post '/pages/guest_sign_in', to: 'pages#guest_sign_in'
+  post '/pages/admin_guest_sign_in', to: 'pages#admin_guest_sign_in'
   get 'top', to: 'pages#top', as: :top 
   resources :projects, shallow: true do
     resources :themes
   end
->>>>>>> develop_crud_05
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2022_10_18_052656) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
